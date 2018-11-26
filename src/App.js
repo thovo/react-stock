3f8e395b64771f4dba129c15cad3b6861a14c725
thovo <votuongtho@gmail.com>
2018-11-26 03:08:53 +0100

    First init commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import BarChart from './BarChart';
+import DataTable from './DataTable';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    // Display an array of data of CAC40 et NASDAG
+    this.state = {
+        index: [1,2,3,4],
+        cac40: [5,3,10,6],
+        nasdaq: [7,8,4,6]
+    };
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+      <div className="stock-application">
+        <header className="app-header">
+          <h1>CAC40 et NASDAQ</h1>
+          <BarChart data={[5,10,1,3]} size={[500,500]} />
+          <DataTable data={this.state}></DataTable>
         </header>
       </div>
     );
---
+++ src/DataTable.js
@@ -0,0 +1,68 @@
+import React, { Component } from 'react';
+import './DataTable.scss';
+
+class DataCell extends Component  {
+
+    constructor(props) {
+        super(props);
+        console.log(props);
+    }
+
+    handleInputChange(event) {
+        const value = event.target.value;
+        // this.setState({value: value});
+        console.log(value);
+    }
+
+    render() {
+        return (
+            <div className="cell">
+                { this.props.editable ?
+                    (
+                        <input type="number" value={this.props.value} onChange={this.handleInputChange}/>
+                    ) : (
+                        <span>{this.props.value}</span>
+                    )
+                }
+            </div>
+        );
+    };
+};
+
+export default class DataTable extends Component  {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <section className="data-table">
+                <h2>Tableau de données</h2>
+                <div className="row first-row">
+                    <div className="cell">Index</div>
+                    {
+                        this.props.data.index.map((value, index) => {
+                            return <DataCell value={value} key={index} editable={false}/>;
+                        })
+                    }
+                </div>
+                <div className="row second-row">
+                    <div className="cell">CAC40</div>
+                    {
+                        this.props.data.cac40.map((value, index) => {
+                            return <DataCell value={value} key={index} editable={true}/>;
+                        })
+                    }
+                </div>
+                <div className="row third-row">
+                    <div className="cell">NASDAQ</div>
+                    {
+                        this.props.data.nasdaq.map((value, index) => {
+                            return <DataCell value={value} key={index} editable={true}/> ;
+                        })
+                    }
+                </div>
+            </section>
+        );
+    };
+};
